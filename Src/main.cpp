6aea6344308643099210b53e10a0d30d67da3596
Esme <esmextreme@proton.me>
2025-12-16 12:24:36 +0100

    Base Keyboard inputs

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <optional>
 #include <iostream>
 #include <vector>
@@ -8,8 +9,13 @@ int main()
 {
     constexpr unsigned WINDOW_W = 1920u;
     constexpr unsigned WINDOW_H = 1080u;
-    auto window = sf::RenderWindow(sf::VideoMode({ 1920u, 1080u }), "SubShootica");
+    auto window = sf::RenderWindow(sf::VideoMode({ WINDOW_W, WINDOW_H }), "SubShootica");
     window.setFramerateLimit(144);
+
+    sf::Vector2u size = window.getSize();
+    auto [Wwidth, Wheight] = size;
+
+
     // berlin-sans-fb-demi-bold
     sf::Font font("BerlinSans.ttf"); // Throws sf::Exception if an error occurs
     // Main text (created once)
@@ -24,12 +30,12 @@ int main()
     button.setFillColor(sf::Color(70, 70, 70));
     button.setOutlineColor(sf::Color::White);
     button.setOutlineThickness(2.f);
-    button.setPosition({ (1920.f - buttonSize.x) / 2.f, (1080.f - buttonSize.y) / 2.f });
+    button.setPosition({ (Wwidth - buttonSize.x) / 2.f, (Wheight - buttonSize.y) / 2.f });
     vector<float> buttonLocation{
-        (((1920.f - buttonSize.x) / 2.f)),
-        (((1920.f - buttonSize.x) / 2.f) + (buttonSize.x)),
-        (((1080.f - buttonSize.y) / 2.f)),
-        (((1080.f - buttonSize.y) / 2.f) + (buttonSize.y)),
+        (((Wwidth - buttonSize.x) / 2.f)),
+        (((Wwidth - buttonSize.x) / 2.f) + (buttonSize.x)),
+        (((Wheight - buttonSize.y) / 2.f)),
+        (((Wheight - buttonSize.y) / 2.f) + (buttonSize.y)),
 };
 
     // Button label
@@ -37,13 +43,14 @@ int main()
     buttonLabel.setString("Change text");
     buttonLabel.setCharacterSize(20);
     buttonLabel.setFillColor(sf::Color::White);
-    buttonLabel.setPosition({ (1920.f - buttonSize.x/2) / 2.f, (1080.f - buttonSize.y/2) / 2.f });
+    buttonLabel.setPosition({ (Wwidth - buttonSize.x/2) / 2.f, (Wheight - buttonSize.y/2) / 2.f });
     vector<string> messages{
     "Hello world",
     "SubShootica!",
     "Button clicked",
     "SFML + CMake"
     };
+	int LabelI = 0;
 
     
     while (window.isOpen())
@@ -57,29 +64,40 @@ int main()
                     window.close();
                 };
 
-            const auto onKeyPressed = [&window](const sf::Event::KeyPressed& keyPressed)
-                {
 
-                    if (keyPressed.scancode == sf::Keyboard::Scancode::Escape)
-                        cout << "Key pressed: " << endl;
-                        window.close();
-
-                };
             if (const auto* mouseButtonPressed = event->getIf<sf::Event::MouseButtonPressed>())
             {
+
                 if (mouseButtonPressed->button == sf::Mouse::Button::Left)
                 {
-                    cout << "the right button was pressed" << endl;
-                    cout << "mouse x: " << mouseButtonPressed->position.x << ", button x:" << buttonLocation[0] << ","<<buttonLocation[1] << ", button y:" << buttonLocation[2] << "," << buttonLocation[3] << endl;
-                    cout << "mouse y: " << mouseButtonPressed->position.y << endl;
-                    if ((mouseButtonPressed->position.x <= buttonLocation[1])&& (mouseButtonPressed->position.y >= buttonLocation[2])&& (mouseButtonPressed->position.y <= buttonLocation[3])) {
-                        buttonLabel.setString("bubek");
+                    cout << "mouse x: " << mouseButtonPressed->position.x << ", mouse y: " << mouseButtonPressed->position.y << endl;
+                    if ((mouseButtonPressed->position.x <= buttonLocation[1]) && (mouseButtonPressed->position.y >= buttonLocation[2]) && (mouseButtonPressed->position.y <= buttonLocation[3])) {
+                        LabelI++;
+                        if (LabelI >= messages.size()) {
+                            LabelI = 0;
+						}
+                        buttonLabel.setString(messages[LabelI]);
                     }
                 }
             }
+
+
             if (event->is<sf::Event::Closed>())
             {
                 window.close();
+            }
+        }
+
+        // Real-time keyboard handling (checks current state; good for continuous movement)
+        if (window.hasFocus())
+        {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+            {
+                cout << "Right key pressed (realtime)" << endl;
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+            {
+                cout << "Left key pressed (realtime)" << endl;
             }
         }
 
