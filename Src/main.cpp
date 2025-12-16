261fb4bab6aa276e0645f7479a094b2daacb25a4
Esme <esmextreme@proton.me>
2025-12-16 13:31:24 +0100

    Switching between Screens

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,18 +4,19 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-int main()
+struct
 {
-    constexpr unsigned WINDOW_W = 1920u;
-    constexpr unsigned WINDOW_H = 1080u;
-    auto window = sf::RenderWindow(sf::VideoMode({ WINDOW_W, WINDOW_H }), "SubShootica");
-    window.setFramerateLimit(144);
+    unsigned WINDOW_W = 1920u;
+    unsigned WINDOW_H = 1080u;
+    int FramerateLimit = 144;
+    bool mainMenuOn = true;
+    bool optionsMenuOn = false;
+} CurrentlyRendering;
+using namespace std;
 
+void MainMenuRenderer(sf::RenderWindow& window) {
     sf::Vector2u size = window.getSize();
     auto [Wwidth, Wheight] = size;
-
-
     // berlin-sans-fb-demi-bold
     sf::Font font("BerlinSans.ttf"); // Throws sf::Exception if an error occurs
     // Main text (created once)
@@ -36,26 +37,109 @@ int main()
         (((Wwidth - buttonSize.x) / 2.f) + (buttonSize.x)),
         (((Wheight - buttonSize.y) / 2.f)),
         (((Wheight - buttonSize.y) / 2.f) + (buttonSize.y)),
-};
+    };
 
     // Button label
     sf::Text buttonLabel(font);
     buttonLabel.setString("Change text");
     buttonLabel.setCharacterSize(20);
     buttonLabel.setFillColor(sf::Color::White);
-    buttonLabel.setPosition({ (Wwidth - buttonSize.x/2) / 2.f, (Wheight - buttonSize.y/2) / 2.f });
+    buttonLabel.setPosition({ (Wwidth - buttonSize.x / 2) / 2.f, (Wheight - buttonSize.y / 2) / 2.f });
     vector<string> messages{
     "Hello world",
     "SubShootica!",
     "Button clicked",
     "SFML + CMake"
     };
-	int LabelI = 0;
-
-    
-    while (window.isOpen())
+    int LabelI = 0;
+    // Use `auto` so the compiler deduces the std::optional<sf::Event> type
+    while (auto event = window.pollEvent())
     {
+        const auto onClose = [&window](const sf::Event::Closed&)
+            {
+                window.close();
+            };
 
+
+        if (const auto* mouseButtonPressed = event->getIf<sf::Event::MouseButtonPressed>())
+        {
+
+            if (mouseButtonPressed->button == sf::Mouse::Button::Left)
+            {
+                cout << "mouse x: " << mouseButtonPressed->position.x << ", mouse y: " << mouseButtonPressed->position.y << endl;
+                if ((mouseButtonPressed->position.x <= buttonLocation[1]) && (mouseButtonPressed->position.y >= buttonLocation[2]) && (mouseButtonPressed->position.y <= buttonLocation[3])) {
+					CurrentlyRendering.mainMenuOn = false;
+					CurrentlyRendering.optionsMenuOn = true;
+                }
+            }
+        }
+
+
+        if (event->is<sf::Event::Closed>())
+        {
+            window.close();
+        }
+    }
+
+    // Real-time keyboard handling (checks current state; good for continuous movement)
+    if (window.hasFocus())
+    {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+        {
+            cout << "Right key pressed (realtime)" << endl;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+        {
+            cout << "Left key pressed (realtime)" << endl;
+        }
+    }
+
+    // Clear the window with a blue background
+    window.clear(sf::Color::Blue);
+    window.draw(mainText);
+    window.draw(button);
+    window.draw(buttonLabel);
+    window.display();
+}
+void OptionsMenuRenderer(sf::RenderWindow& window) {
+
+        sf::Vector2u size = window.getSize();
+        auto [Wwidth, Wheight] = size;
+        // berlin-sans-fb-demi-bold
+        sf::Font font("BerlinSans.ttf"); // Throws sf::Exception if an error occurs
+        // Main text (created once)
+        sf::Text mainText(font);
+        mainText.setString("Options");
+        mainText.setCharacterSize(24);
+        mainText.setFillColor(sf::Color(255, 165, 0)); // orange
+
+        // Button rectangle
+        sf::Vector2f buttonSize{ 200.f, 60.f };
+        sf::RectangleShape button(buttonSize);
+        button.setFillColor(sf::Color(70, 70, 70));
+        button.setOutlineColor(sf::Color::White);
+        button.setOutlineThickness(2.f);
+        button.setPosition({ (Wwidth - buttonSize.x) / 2.f, (Wheight - buttonSize.y) / 2.f });
+        vector<float> buttonLocation{
+            (((Wwidth - buttonSize.x) / 2.f)),
+            (((Wwidth - buttonSize.x) / 2.f) + (buttonSize.x)),
+            (((Wheight - buttonSize.y) / 2.f)),
+            (((Wheight - buttonSize.y) / 2.f) + (buttonSize.y)),
+        };
+
+        // Button label
+        sf::Text buttonLabel(font);
+        buttonLabel.setString("Change text");
+        buttonLabel.setCharacterSize(20);
+        buttonLabel.setFillColor(sf::Color::White);
+        buttonLabel.setPosition({ (Wwidth - buttonSize.x / 2) / 2.f, (Wheight - buttonSize.y / 2) / 2.f });
+        vector<string> messages{
+        "Hello world",
+        "SubShootica!",
+        "Button clicked",
+        "SFML + CMake"
+        };
+        int LabelI = 0;
         // Use `auto` so the compiler deduces the std::optional<sf::Event> type
         while (auto event = window.pollEvent())
         {
@@ -72,11 +156,8 @@ int main()
                 {
                     cout << "mouse x: " << mouseButtonPressed->position.x << ", mouse y: " << mouseButtonPressed->position.y << endl;
                     if ((mouseButtonPressed->position.x <= buttonLocation[1]) && (mouseButtonPressed->position.y >= buttonLocation[2]) && (mouseButtonPressed->position.y <= buttonLocation[3])) {
-                        LabelI++;
-                        if (LabelI >= messages.size()) {
-                            LabelI = 0;
-						}
-                        buttonLabel.setString(messages[LabelI]);
+                        CurrentlyRendering.mainMenuOn = true;
+                        CurrentlyRendering.optionsMenuOn = false;
                     }
                 }
             }
@@ -108,4 +189,27 @@ int main()
         window.draw(buttonLabel);
         window.display();
     }
+
+
+int main()
+{
+    auto window = sf::RenderWindow(sf::VideoMode({ CurrentlyRendering.WINDOW_W, CurrentlyRendering.WINDOW_H }), "SubShootica");
+    window.setFramerateLimit(CurrentlyRendering.FramerateLimit);
+
+    sf::Vector2u size = window.getSize();
+    auto [Wwidth, Wheight] = size;
+
+    while (window.isOpen())
+    {
+
+
+        if (CurrentlyRendering.mainMenuOn) {
+            MainMenuRenderer(window);
+        }
+        if (CurrentlyRendering.optionsMenuOn) {
+            OptionsMenuRenderer(window);
+        }
+
+    }
+
 }
